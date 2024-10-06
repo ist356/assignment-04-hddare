f8f19c48ad07f3c365ac1a5762314c1100e61118
hddare <hddare@syr.edu>
2024-10-06 18:36:04 -0400

    pandaslib.py: added a new function to the library

--- code/pandaslib.py
+++ code/pandaslib.py
@@ -5,46 +5,28 @@ SOLUTION FILE!!!!
 import pandas as pd
 
 def get_column_names(df : pd.DataFrame) -> list[str]:
-    '''
-    Get all column names of a pandas dataframe df
-    Returns the names as a list of string
-    '''
-    pass # todo: replace this line and add your code
+   return list(df.columns)
 
 
 def get_columns_of_type(df : pd.DataFrame, numpy_type: any) -> list[str]:
-    '''
-    Return the column names of a pandas dataframe only when 
-    the values in the column match the numpy_type
-    '''
-    pass # todo: replace this line and add your code
+    return [col for col in df.columns if df[col].dtype == numpy_type]
 
 
 def get_unique_values(df : pd.DataFrame, column_name: str) -> list:
-    '''
-    Get a list of unique values of a column in a pandas dataframe
-    '''
-    pass # todo: replace this line and add your code
+    return df[column_name].unique().tolist()
 
 def get_file_extension(file_path : str) -> str:
-    '''
-    Return the file extension of a file_path for example:
-    '/some/file/data.csv' -> 'csv'
-    '/home/important_grades.xlsx' -> 'xlsx'
-    'countries.json' -> 'json'
-
-    '''
-    pass # todo: replace this line and add your code
+    return file_path.split('.')[-1]
 
 def load_file(file_path: str, ext: str) -> pd.DataFrame:
-    '''
-    Load a file into a pandas dataframe assumed the file type from the extension
-    return a pandas dataframe
-    only suppose csv, excel and json file extensions
-    - when csv assume first row is header
-    - when json assume record-oriented data
-    '''
-    pass # todo: replace this line and add your code
+    if ext == 'csv':
+        return pd.read_csv(file_path, header=0)
+    elif ext in ['xls', 'xlsx']:
+        return pd.read_excel(file_path)
+    elif ext == 'json':
+        return pd.read_json(file_path, orient='records')
+    else:
+        raise ValueError(f"Unsupported file extension: {ext}")
 
 if __name__ == '__main__':
     df = pd.DataFrame({ 
